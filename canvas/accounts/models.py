2661db553be138716e99c86144c25bbde47c8880
c03rcion <areeves9@icloud.com>
2017-03-19 16:39:23 -0700

    added two functions: create_user_profile and save_user_profile to models.py

--- canvas/accounts/models.py
+++ canvas/accounts/models.py
@@ -4,9 +4,18 @@ from django.conf import settings
 from django.contrib.auth.models import User
 
 from django.db.models.signals import post_save
-from django.dispatch import receiver 
+from django.dispatch import receiver
 
 # Create your models here.
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.created(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     bio = models.TextField(max_length=1000, blank=True)
