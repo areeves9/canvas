9132aef99d9f27a46309f8b92dfbbb75efdf9b60
c03rcion <areeves9@icloud.com>
2017-03-19 15:41:26 -0700

    created accounts urlpatten 'register' with generic CreateView and UserCreationForm

---
+++ canvas/accounts/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include, url
+from django.contrib.auth.forms import UserCreationForm
+
+from . import views
+from django.views.generic.edit import CreateView
+
+urlpatterns = [
+    url(r'^register', CreateView.as_view(
+        template_name='registration/registration_form.html',
+        form_class=UserCreationForm,
+        success_url='/profile/'
+    ), name='register')
+]
