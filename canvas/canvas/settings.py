040ce0b8967a52507e2f1f7ab2c83502dfe2eb6f
c03rcion <areeves9@icloud.com>
2017-03-19 15:26:36 -0700

    configured settings databse settings for postgresql

--- canvas/canvas/settings.py
+++ canvas/canvas/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'canvas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'canvas',
+        'USER': 'toker',
+        'PASSWORD': 'milk1234',
+        'HOST': 'localhost',
+        'PORT': '',
+
     }
 }
 
