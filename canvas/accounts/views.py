8e9cbe1c9331bd5e5542d4de1ad3ee02bb904532
c03rcion <areeves9@icloud.com>
2017-03-19 16:08:54 -0700

    created profile view, imported login_requred decorator

--- canvas/accounts/views.py
+++ canvas/accounts/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
+@login_required
 def profile(request):
-    pass
+    user = request.user
+    context = {
+        "user": user,
+    }
+    return render(request, "accounts/profile.html", context)
