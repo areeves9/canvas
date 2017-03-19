b4ac2298d2a680311a723e3d5bd5c2fbfd6d31b9
c03rcion <areeves9@icloud.com>
2017-03-19 15:34:40 -0700

    created Profile model

--- canvas/accounts/models.py
+++ canvas/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+from django.conf import settings
+from django.contrib.auth.models import User
+
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=1000, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birthdate = models.DateField(null=True, blank=True)
