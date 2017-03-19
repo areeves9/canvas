c79fe86338d5b6c04f4221aa5a3314c28f392898
c03rcion <areeves9@icloud.com>
2017-03-19 16:36:59 -0700

    imported post_save and receiver in accounts:models

--- canvas/accounts/models.py
+++ canvas/accounts/models.py
@@ -3,6 +3,9 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver 
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
