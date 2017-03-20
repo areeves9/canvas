1a1525f9664ddd2ea945ee980d9547b73784fc5c
c03rcion <areeves9@icloud.com>
2017-03-19 21:31:34 -0700

    created model Review in reviews

--- canvas/reviews/models.py
+++ canvas/reviews/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+def upload_location(instance, filename):
+    return "%s/%s" % (instance.user, filename)
+
 class Strain(models.Model):
     name = models.CharField(max_length=60)
     summary = models.TextField(blank=True, null=True)
@@ -11,4 +16,33 @@ class Strain(models.Model):
 class Review(models.Model):
     title = models.CharField(max_length=35)
     content = models.TextField(max_length=500)
-    photo = 
+    strain = models.ForeignKey(Strain)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,  related_name="user" ,default=1)
+    photo = models.ImageField(
+        upload_to=upload_location,
+        blank=True,
+        null=True,
+        height_field="height_field",
+        width_field="width_field",
+    )
+    height_field = models.IntegerField(default=0)
+    width_field = models.IntegerField(default=0)
+    FLOWER = 'FLOWER'
+    EXTRACT = 'EXTRACT'
+    EDIBLE = 'EDIBLE'
+    METHOD_CHOICES = (
+        (FLOWER, 'FLOWER'),
+        (EXTRACT, 'EXTRACT'),
+        (EDIBLE, 'EDIBLE'),
+    )
+    method = models.CharField(
+        max_length=20,
+        choices=METHOD_CHOICES,
+        default='FLOWER,
+        blank=False
+    )
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return self.title
