1f4972410b4b06617b1ff5475a28c6254f74b9cc
c03rcion <areeves9@icloud.com>
2017-03-19 22:20:18 -0700

    added urlpattern and view for review list

--- canvas/reviews/urls.py
+++ canvas/reviews/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.reviews, name="reviews"),
     url(r'^strains$', views.strains, name="strains")
 ]
--- canvas/reviews/views.py
+++ canvas/reviews/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
-from reviews.models import Strain
+from reviews.models import Strain, Review
 # Create your views here.
+
+def reviews(request):
+    reviews = Review.objects.all()
+    context = {
+        "reviews": reviews,
+    }
+    return render(request, "reviews/reviews.html", context)
+
 def strains(request):
     strains = Strain.objects.all()
     context = {
