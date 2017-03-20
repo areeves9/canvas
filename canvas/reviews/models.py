3255735a0b61953a38cda96a3b4ba83fe8159099
c03rcion <areeves9@icloud.com>
2017-03-19 20:41:17 -0700

    created model for reviews, Strain

--- canvas/reviews/models.py
+++ canvas/reviews/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Strain(models.Model):
+    name = models.CharField(max_length=60)
+    summary = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Review(models.Model):
+    title = models.CharField(max_length=35)
+    content = models.TextField(max_length=500)
+    photo = 
