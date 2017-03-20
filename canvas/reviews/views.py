a0d3877521f074faec275fc409f94101ef47635b
c03rcion <areeves9@icloud.com>
2017-03-19 22:02:21 -0700

    created view strains in reviews

--- canvas/reviews/views.py
+++ canvas/reviews/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from reviews.models import Strain
 # Create your views here.
+def strains(request):
+    strains = Strain.objects.all()
+    context = {}
+    return render(request, "reviews/strains.html", context)
