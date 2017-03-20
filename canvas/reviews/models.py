5e3407c81007b6705700a21edbcb36fc7c1f83e9
c03rcion <areeves9@icloud.com>
2017-03-19 22:49:22 -0700

    added method get_absolute_url to Review

--- canvas/reviews/models.py
+++ canvas/reviews/models.py
@@ -45,5 +45,8 @@ class Review(models.Model):
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 
+    def get_absolute_url(self):
+        return reverse("reviews:detail", kwargs={"id": self.id})
+
     def __str__(self):
         return self.title
