019ed3fca83aef0bae97816ecda1b854e6f05ab8
c03rcion <areeves9@icloud.com>
2017-03-26 11:08:47 -0700

    added icons and birthdate to profile.html

--- canvas/canvas/settings.py
+++ canvas/canvas/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join("accounts", "static")
+    os.path.join("reviews", "static")
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static_production")
