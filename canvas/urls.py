b1840afdf1db24fcf4c99ec9b5d16a99b3520aae
c03rcion <areeves9@icloud.com>
2017-04-25 11:14:05 -0700

    base.STATIC_URL

--- canvas/urls.py
+++ canvas/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
 ]
 
 if base.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(base.STATIC_URL, document_root=base.STATIC_ROOT)
+    urlpatterns += static(base.MEDIA_URL, document_root=base.MEDIA_ROOT)
