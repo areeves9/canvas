c284b278d3e92937681e32d2061d417e9c89b5fc
c03rcion <areeves9@icloud.com>
2017-04-30 00:35:51 -0700

    rdy

---
+++ canvas/local_settings.py
@@ -0,0 +1,31 @@
+import os
+from django.core.urlresolvers import reverse_lazy
+
+from .settings import BASE_DIR
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'canvas',
+        'USER': 'toker',
+        'PASSWORD': os.environ['DB_CANVAS_PW'],
+        'HOST': 'localhost',
+        'PORT': '5432',
+
+    }
+}
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join("reviews", "static")
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static_production")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media_production")
