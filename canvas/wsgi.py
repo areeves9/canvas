8c3972e548fbbc87ae3435ee16c8967c811adeaa
c03rcion <areeves9@icloud.com>
2017-04-30 00:27:19 -0700

    all

--- canvas/wsgi.py
+++ canvas/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "canvas.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "canvas.production")
 
 application = get_wsgi_application()
