1aaaaeb608d12bf9ca4b41fbaceb31ff34ff25a8
c03rcion <areeves9@icloud.com>
2017-04-30 01:07:18 -0700

    helpiax

--- canvas/settings.py
+++ canvas/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ.get('CANVAS_KEY', '')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://canvasreviews.herokuapp.com/,']
+ALLOWED_HOSTS = ['canvasreviews.herokuapp.com']
 
 
 # Application definition
