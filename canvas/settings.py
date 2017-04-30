9ecbf54f46b748d20e27be4fa712ca119366a152
c03rcion <areeves9@icloud.com>
2017-04-30 02:11:56 -0700

    comment out

--- canvas/settings.py
+++ canvas/settings.py
@@ -128,10 +128,12 @@ LOGIN_REDIRECT_URL = reverse_lazy('accounts:profile')
 # Have to move this try statement before and after the following
 # debug check, need to refactor so the block under if DEBUG check
 # runs to update MEDIA_URL and STATIC_URL
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+
+# try:
+#     from .local_settings import *
+# except ImportError:
+#     pass
+
 
 if DEBUG==False:
     STATICFILES_LOCATION = 'static'
