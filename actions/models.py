28e7557b1e12804b278007d1eeaaf92e409029a3
c03rcion <areeves9@icloud.com>
2017-05-12 23:08:51 -0700

    Action model

--- actions/models.py
+++ actions/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Action(models.Model):
+    user = models.ForeignKey(User, related_name='actions', db_index=True)
+    verb = models.CharField(max_length=225)
+    created = models.DateTimeField(auto_add_now=True, db_index=True)
+
+    class Meta:
+        ordering = ('-created',)
