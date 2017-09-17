ba43993208c4616549f9dd1a1afad836a5abe473
c03rcion <areeves9@icloud.com>
2017-09-17 12:37:36 -0700

    settings

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.reviews, name="reviews"),
+    url(r'reviews/^$', views.reviews, name="reviews"),
     url(r'^(?P<id>\d+)/$', views.review_detail, name="detail"),
     url(r'^(?P<id>\d+)/update/$', views.review_update, name="update"),
     url(r'^like/$', views.review_like, name="like"),
