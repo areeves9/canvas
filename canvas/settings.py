ba80df6c5575a01cfcb4b3580376a12524397df4
c03rcion <areeves9@icloud.com>
2017-11-05 21:26:55 -0800

    added url pattern for search in project lvl dir, added html file for search template

--- canvas/settings.py
+++ canvas/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'storages',
     'gunicorn',
     'actions',
-    'pysolr',
     'haystack',
 ]
 
--- canvas/urls.py
+++ canvas/urls.py
@@ -21,13 +21,13 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 
-
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('django.contrib.auth.urls', namespace='auth')),
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^reviews/', include('reviews.urls', namespace='reviews')),
+    url(r'^search/', include('haystack.urls')),
 ]
 
 if settings.DEBUG==True:
