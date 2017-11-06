8ba67c2974b28e34fafa1cf30068481ebb65fe9b
c03rcion <areeves9@icloud.com>
2017-11-05 21:16:31 -0800

    created class ReviewIndex

--- reviews/search_indexes.py
+++ reviews/search_indexes.py
@@ -1,2 +1,13 @@
 from haystack import indexes
 from reviews.models import Review
+
+
+class ReviewIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    name = indexes.CharField(model_attr='name')
+
+    def get_model(self):
+        return Review
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
