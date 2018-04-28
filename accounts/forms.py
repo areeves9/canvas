b313062f919976f0fcda8614ecc1b4d3cea6ad0f
c03rcion <areeves9@icloud.com>
2018-04-28 16:50:49 -0700

    created UserRegisterForm in forms.py:accounts

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,23 +1,34 @@
 from django import forms
 
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import User, UserCreationForm
 from accounts.models import Profile
 
+
 class UserForm(forms.ModelForm):
+    # email = forms.EmailField(max_length=200, help_text='Required')
     class Meta:
         model = User
-        fields = [
+        fields = (
             "first_name",
             "last_name",
             "email",
-        ]
+        )
+
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = [
+        fields = (
             "bio",
             "location",
             "birthdate",
             "photo",
-        ]
+        )
+
+
+class UserRegisterForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name',)
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import include, url
-from django.contrib.auth.forms import UserCreationForm
+from accounts.forms import UserForm, UserRegisterForm
+# from django.contrib.auth.forms import UserCreationForm
 
 from . import views
 from django.views.generic.edit import CreateView
@@ -10,7 +11,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^register/$', CreateView.as_view(
         template_name='registration/registration_form.html',
-        form_class=UserCreationForm,
+        form_class=UserRegisterForm,
         success_url='/account/login/',
     ), name='register'),
     url(r'^password_change/$', auth_views.password_change, {'template_name': 'registration/password_change.html','post_change_redirect': 'accounts:password_change_done'}, name="password_change"),
