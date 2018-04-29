7cbeb2a157dd30253b94014050912410368933fe
c03rcion <areeves9@icloud.com>
2018-04-29 14:25:12 -0700

    template for email share review

--- canvas/settings.py
+++ canvas/settings.py
@@ -23,6 +23,13 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['canvasreviews.herokuapp.com', '.herokuapp.com']
 
+# Email settings
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'bedouinalchemist@gmail.com'
+EMAIL_HOST_PASSWORD = '***REMOVED***'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 
 # Application definition
 
