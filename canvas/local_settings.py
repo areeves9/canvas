b26b833d8357e2a52f981f69318e879904922af2
c03rcion <areeves9@icloud.com>
2018-04-29 18:48:25 -0700

    settings

--- canvas/local_settings.py
+++ canvas/local_settings.py
@@ -7,6 +7,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+HAYSTACK_CONNECTIONS = {
+       'default': {
+           'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+           'URL': 'http://127.0.0.1:9200/',
+           'INDEX_NAME': 'haystack_reviews',
+       },
+}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
