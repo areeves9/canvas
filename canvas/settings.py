861830f03d67d6ce018115f49c0d560c02836e07
c03rcion <areeves9@icloud.com>
2020-05-17 22:12:32 -0700

    added ui

--- canvas/settings.py
+++ canvas/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 SECRET_KEY = os.environ.get('CANVAS_KEY', '')
 
-DEBUG = False
+DEBUG = os.environ.get('CANVAS_ENV')
 
 ALLOWED_HOSTS = ['canvasreviews.herokuapp.com', '.herokuapp.com']
 
