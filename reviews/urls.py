f12e51b3e7c3b7e87cf93a90d22b5b0d962a8676
Andrew Reeves <areeves9@icloud.com>
2021-05-02 18:43:02 -0700

    reviews urls.py import path, changed url names, updated strain detail template

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,4 +1,4 @@
-from django.urls import re_path
+from django.urls import path
 from django.views.generic import TemplateView
 
 
@@ -7,18 +7,18 @@ from . import views
 app_name = 'reviews'
 
 urlpatterns = [
-    re_path(r'^$', views.reviews, name="reviews"),
-    re_path(r'^(?P<id>\d+)/$', views.review_detail, name="review_detail"),
-    re_path(r'^strain/(?P<id>\d+)/$', views.strain_reviews, name="strain_reviews"),
-    re_path(r'^(?P<id>\d+)/update/$', views.review_update, name="review_update"),
-    re_path(r'^(?P<id>\d+)/delete/$', views.review_delete, name="review_delete"),
-    re_path(r'^(?P<id>\d+)/share/$', views.review_share, name="share_review"),
-    re_path(r'^review-like/$', views.review_like, name="review_like"),
-    re_path(r'^strains/$', views.strains, name="strains"),
-    re_path(r'^strains/(?P<id>\d+)/$', views.strain_detail, name="strain_detail"),
-    re_path(r'^strains/(?P<id>\d+)/review/$', views.strain_review, name="strain_review"),
-    re_path(r'^strain-like/$', views.strain_like, name="strain_like"),
-    re_path(r'^strains/(?P<id>\d+)/share/$', views.strain_share, name="share_strain"),
-    re_path(r'^reviews-search/$', TemplateView.as_view(template_name='reviews/reviews_search.html'), name='reviews_search'),
-    re_path(r'^autocomplete/$', views.autocomplete, name='autocomplete'),
+    path('', views.reviews, name="reviews"),
+    path('<id>', views.review_detail, name="review_detail"),
+    path('^strain/<id>', views.strain_reviews, name="strain_reviews"),
+    path('<id>/update', views.review_update, name="review_update"),
+    path('<id>/delete', views.review_delete, name="review_delete"),
+    path('<id>/share', views.review_share, name="share_review"),
+    path('like/review', views.review_like, name="review_like"),
+    path('strains/list', views.strains, name="strains"),
+    path('strains/<id>', views.strain_detail, name="strain_detail"),
+    path('strains/<id>/review', views.strain_review, name="strain_review"),
+    path('like/strain', views.strain_like, name="strain_like"),
+    path('strains/<id>/share', views.strain_share, name="share_strain"),
+    path('reviews-search', TemplateView.as_view(template_name='reviews/reviews_search.html'), name='reviews_search'),
+    path('autocomplete', views.autocomplete, name='autocomplete'),
 ]
