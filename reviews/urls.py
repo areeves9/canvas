cdd9014db34f45c7a9b017cf225aed093b292f1a
Andrew Reeves <areeves9@icloud.com>
2021-05-02 20:37:30 -0700

    revised user review templates in strain detail

--- reviews/urls.py
+++ reviews/urls.py
@@ -9,6 +9,7 @@ app_name = 'reviews'
 urlpatterns = [
     path('', views.reviews, name="reviews"),
     path('<id>', views.review_detail, name="review_detail"),
+    # replace this with a query param to filter reviews by strain
     path('^strain/<id>', views.strain_reviews, name="strain_reviews"),
     path('<id>/update', views.review_update, name="review_update"),
     path('<id>/delete', views.review_delete, name="review_delete"),
@@ -19,6 +20,5 @@ urlpatterns = [
     path('strains/<id>/review', views.strain_review, name="strain_review"),
     path('like/strain', views.strain_like, name="strain_like"),
     path('strains/<id>/share', views.strain_share, name="share_strain"),
-    path('reviews-search', TemplateView.as_view(template_name='reviews/reviews_search.html'), name='reviews_search'),
-    path('autocomplete', views.autocomplete, name='autocomplete'),
+    path('search/', TemplateView.as_view(template_name='reviews/reviews_search.html'), name='reviews_search'),
 ]
