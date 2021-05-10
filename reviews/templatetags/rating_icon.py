4931037a350e35404e2a60f54e4f1c050a9f8960
Andrew Reeves <areeves9@icloud.com>
2021-05-09 17:49:49 -0700

    rating icon fractional stars

--- reviews/templatetags/rating_icon.py
+++ reviews/templatetags/rating_icon.py
@@ -1,3 +1,4 @@
+import math
 from django import template
 from django.utils.safestring import mark_safe
 
@@ -6,4 +7,13 @@ register = template.Library()
 
 @register.filter
 def rating_icon(rating):
-    return mark_safe('<i class="text-secondary fas fa-star"></i>' * rating)
+    if rating == '':
+        return ''
+    else:
+        print(type(rating))
+        new_rating = math.modf(float(rating))
+        print(new_rating)
+        if new_rating[0] >= 0.5 and new_rating[0] <= 0.9:
+            return mark_safe('<i class="text-secondary fas fa-star"></i>' * int(rating)) + mark_safe('<i class="text-secondary fas fa-star-half-alt"></i>')
+        else:
+            return mark_safe('<i class="text-secondary fas fa-star"></i>' * int(rating)) 
