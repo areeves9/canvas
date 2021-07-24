b967bebe7f7a2625b712c7eaeb18522083b6b9cc
Andrew Reeves <areeves9@icloud.com>
2021-07-23 19:45:24 -0700

    database settings

--- canvas/settings.py
+++ canvas/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 import sentry_sdk
 from urllib.parse import urlparse
 from sentry_sdk.integrations.django import DjangoIntegration
@@ -105,22 +106,15 @@ HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DB_CANVAS_NAME'],
-        'USER': os.environ['DB_CANVAS_USER'],
-        'PASSWORD': os.environ['DB_CANVAS_PW'],
-        'HOST': 'localhost',
-        'PORT': '5432',
-
     }
 }
 
-if DEBUG is False:
-    import dj_database_url
-    DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
