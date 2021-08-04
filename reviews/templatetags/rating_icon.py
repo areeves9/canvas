2bcf539ae44d03427e3eae1720c8431b0ccf3e9b
Andrew Reeves <areeves9@icloud.com>
2021-08-03 23:12:23 -0700

    minor template changes

--- reviews/templatetags/rating_icon.py
+++ reviews/templatetags/rating_icon.py
@@ -14,6 +14,6 @@ def rating_icon(rating):
         rem = 5 - new_rating[1]
         empty_stars = rem - 1
         if new_rating[0] >= 0.5 and new_rating[0] <= 0.9:
-            return mark_safe('<i style="color: #6EB257" class="fas fa-star"></i>' * int(new_rating[1])) + mark_safe('<i style="color: #6EB257" class="fas fa-star-half-alt"></i>') + mark_safe('<i  style="color: #6EB257" class="far fa-star"></i>' * int(empty_stars))
+            return mark_safe('<i  class="emerald fas fa-star"></i>' * int(new_rating[1])) + mark_safe('<i  class="emerald emerald fas fa-star-half-alt"></i>') + mark_safe('<i   class="emerald far fa-star"></i>' * int(empty_stars))
         else:
-            return mark_safe('<i style="color: #6EB257" class="fas fa-star"></i>' * int(new_rating[1])) + mark_safe('<i  style="color: #6EB257" class="far fa-star"></i>' * int(rem))
+            return mark_safe('<i  class="emerald fas fa-star"></i>' * int(new_rating[1])) + mark_safe('<i class="emerald far fa-star"></i>' * int(rem))
