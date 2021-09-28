a46e17fd0246c8b504c028ba26649574b55da5d2
Andrew Reeves <areeves9@icloud.com>
2021-09-28 12:40:41 -0700

    added m2m field on Review (flavors); updated templates for photo_url

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 
-from reviews.models import Review, Strain, Comment
+from reviews.models import Review, Strain, Comment, Flavor
 # Register your models here.
 
 
+class FlavorModelAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    list_display_links = ["name"]
+    list_filter = ["name"]
+
+    class Meta:
+        model = Flavor
+
+
 class ReviewModelAdmin(admin.ModelAdmin):
-    list_display = ["title", "user", "strain", "rating", "timestamp", "photo"]
+    list_display = ["title", "user", "strain", "rating", "timestamp", "photo", "get_flavors"]
     list_display_links = ["title"]
     list_filter = ["strain", "user", "rating"]
 
@@ -31,6 +40,7 @@ class CommentModelAdmin(admin.ModelAdmin):
         model = Comment
 
 
+admin.site.register(Flavor, FlavorModelAdmin)
 admin.site.register(Review, ReviewModelAdmin)
 admin.site.register(Strain, StrainModelAdmin)
 admin.site.register(Comment, CommentModelAdmin)
--- reviews/widgets.py
+++ reviews/widgets.py
@@ -1,6 +1,11 @@
 from django import forms
 
 
+class FlavorSelectWidget(forms.widgets.CheckboxSelectMultiple):
+    template_name = 'partials/reviews/widgets/checkbox_select.html'
+    option_template_name = 'partials/reviews/widgets/checkbox_option.html'
+
+
 class MethodSelectWidget(forms.widgets.RadioSelect):
     template_name = 'partials/reviews/widgets/method_select_widget.html'
     option_template_name = 'partials/reviews/widgets/method_select_option.html'
