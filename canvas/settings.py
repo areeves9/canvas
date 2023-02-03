74c73a487071d85a766eb449331aa2391cc54303
Andrew Reeves <areeves9@icloud.com>
2023-02-03 11:03:10 -0800

    AWS ACL none

--- canvas/settings.py
+++ canvas/settings.py
@@ -152,6 +152,7 @@ LOGOUT_REDIRECT_URL = "home"
 
 # Static files
 if os.environ.get("ENABLE_S3"):
+    AWS_DEFAULT_ACL = None
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
